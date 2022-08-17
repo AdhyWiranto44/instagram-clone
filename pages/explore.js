bc4c7d24f28c2547744a554f46b106109764cfce
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-08-17 15:54:09 +0700

    feat: explore page, show posts

--- pages/explore.js
+++ pages/explore.js
@@ -1,14 +1,34 @@
-import Script from "next/script";
 import Navbar from "../components/navbar";
-import PostModal from "../components/post_modal";
 
 export default function Explore() {
   return (
     <>
       <div className="mb-10">
         <Navbar />
-        <div id="main" className="container-fluid lg:px-40 xl:px-56 mt-0 md:mt-6">
-          <h1>Explore Page</h1>
+        <div id="main" className="container mx-auto lg:px-36 mt-2 p-1">
+          <div className="explore_posts">
+            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1" onClick={() => console.log("ter-click~")}>
+              <img className='hover:border-2 border-black' src='post_2.jpg' />
+            </div>
+            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+              <img className='hover:border-2 border-black' src='background.jpg' />
+            </div>
+            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+              <img className='hover:border-2 border-black' src='mina.jpg' />
+            </div>
+            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+              <img className='hover:border-2 border-black' src='post_3.jpg' />
+            </div>
+            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+              <img className='hover:border-2 border-black' src='post_1.jpg' />
+            </div>
+            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+              <img className='hover:border-2 border-black' src='tzuyu.jpg' />
+            </div>
+            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+              <img className='hover:border-2 border-black' src='dahyun.jpg' />
+            </div>
+          </div>
         </div>
       </div>
     </>
