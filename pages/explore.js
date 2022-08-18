46231bdd170f790601be708fd7f2d2738690d448
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-08-18 13:07:16 +0700

    fix: responsivity explore page

--- pages/explore.js
+++ pages/explore.js
@@ -5,27 +5,27 @@ export default function Explore() {
     <>
       <div className="mb-10">
         <Navbar />
-        <div id="main" className="container mx-auto lg:px-36 mt-2 p-1">
+        <div id="main" className="container mx-auto lg:px-36 xl:px-44 mt-2 p-1">
           <div className="explore_posts">
-            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1" onClick={() => console.log("ter-click~")}>
+            <div id="post" className="w-4/12 inline-block p-1 md:p-2 lg:p-4 cursor-pointer -mt-1" onClick={() => console.log("ter-click~")}>
               <img className='hover:border-2 border-black' src='post_2.jpg' />
             </div>
-            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+            <div id="post" className="w-4/12 inline-block p-1 md:p-2 lg:p-4 cursor-pointer -mt-1">
               <img className='hover:border-2 border-black' src='background.jpg' />
             </div>
-            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+            <div id="post" className="w-4/12 inline-block p-1 md:p-2 lg:p-4 cursor-pointer -mt-1">
               <img className='hover:border-2 border-black' src='mina.jpg' />
             </div>
-            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+            <div id="post" className="w-4/12 inline-block p-1 md:p-2 lg:p-4 cursor-pointer -mt-1">
               <img className='hover:border-2 border-black' src='post_3.jpg' />
             </div>
-            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+            <div id="post" className="w-4/12 inline-block p-1 md:p-2 lg:p-4 cursor-pointer -mt-1">
               <img className='hover:border-2 border-black' src='post_1.jpg' />
             </div>
-            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+            <div id="post" className="w-4/12 inline-block p-1 md:p-2 lg:p-4 cursor-pointer -mt-1">
               <img className='hover:border-2 border-black' src='tzuyu.jpg' />
             </div>
-            <div id="post" className="w-4/12 inline-block p-1 cursor-pointer -mt-1">
+            <div id="post" className="w-4/12 inline-block p-1 md:p-2 lg:p-4 cursor-pointer -mt-1">
               <img className='hover:border-2 border-black' src='dahyun.jpg' />
             </div>
           </div>
